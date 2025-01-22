fd094f36377b4024fda536abd646330dd7bebef3
Faheem <mfaheemamin4797@gmail.com>
2025-01-22 07:58:56 +0500

    minor changes

--- lib/features/users_list/data/datasources/user_remote_datasource.dart
+++ lib/features/users_list/data/datasources/user_remote_datasource.dart
@@ -1,10 +1,9 @@
 import 'package:bilytica_assignment/core/constants/constants.dart';
 import 'package:bilytica_assignment/core/error/server_exception.dart';
-import 'package:bilytica_assignment/features/users_list/data/models/user_model.dart';
 import 'package:dio/dio.dart';
 
 abstract interface class UserRemoteDatasource {
-  Future<List<UserModel>> fetchUsers(
+  Future<List<dynamic>> fetchUsers(
     int page,
   );
 }
@@ -15,7 +14,7 @@ class UserRemoteDatasourceImpl implements UserRemoteDatasource {
   UserRemoteDatasourceImpl({required this.dio});
 
   @override
-  Future<List<UserModel>> fetchUsers(int page) async {
+  Future<List<dynamic>> fetchUsers(int page) async {
     try {
       final response = await dio.get(
         Constants.baseApiUrl,
@@ -28,7 +27,7 @@ class UserRemoteDatasourceImpl implements UserRemoteDatasource {
       if (response.statusCode == 200) {
         final responseData = response.data['results'] as List<dynamic>;
 
-        return responseData.map((json) => UserModel.fromMap(json)).toList();
+        return responseData;
       } else {
         throw ServerException(
             'Error fetching users. Status code: ${response.statusCode}');
--- lib/features/users_list/data/repository/user_repository_impl.dart
+++ lib/features/users_list/data/repository/user_repository_impl.dart
@@ -3,6 +3,7 @@ import 'package:bilytica_assignment/core/error/failure.dart';
 import 'package:bilytica_assignment/core/error/server_exception.dart';
 import 'package:bilytica_assignment/core/network/connection_checker.dart';
 import 'package:bilytica_assignment/features/users_list/data/datasources/user_remote_datasource.dart';
+import 'package:bilytica_assignment/features/users_list/data/models/user_model.dart';
 import 'package:bilytica_assignment/features/users_list/domain/entities/user.dart';
 import 'package:bilytica_assignment/features/users_list/domain/repository/user_repository.dart';
 import 'package:fpdart/fpdart.dart';
@@ -21,7 +22,9 @@ class UserRepositoryImpl implements UserRepository {
       if (!await connectionChecker.isConnected) {
         return left(const Failure(Constants.noConnectionErrorMessage));
       }
-      final listOfUsers = await userRemoteDatasource.fetchUsers(page);
+      final responseData = await userRemoteDatasource.fetchUsers(page);
+      final listOfUsers =
+          responseData.map((json) => UserModel.fromMap(json)).toList();
       return right(listOfUsers);
     } on ServerException catch (e) {
       return left(Failure(e.message));
